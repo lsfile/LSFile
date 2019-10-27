e2104d5b35c0b43cc96bbe5de49700e6076e0143
lsfile <57066561+lsfile@users.noreply.github.com>
2019-10-27 15:45:17 +0000

    Footer

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,5 +1,6 @@
 <hr>
 
 <div class="footer">
-    Powered by, <a href="http://www.directorylister.com">Directory Lister</a>
+    ©<script type="text/javascript">document.write(new Date().getFullYear());</script> <strong>LSFile Project</strong> | Connect with <a href="https://github.com/lsfile" title="GitHub LSFile" target="_blank">GitHub</a>
+    Powered by <a href="https://heroku.com" title="Heroku" target="_blank">Heroku</a>
 </div>
