7d7461ce3f65a433473159514e616a954473a69c
Eve <57066561+lsfile@users.noreply.github.com>
2019-10-27 16:50:22 +0000

    Update index.php

--- resources/themes/bootstrap/index.php
+++ resources/themes/bootstrap/index.php
@@ -4,7 +4,7 @@
 
     <head>
 
-        <title><?php echo $lister->getPath(); ?> – LSFile Project</title>
+        <title><?php echo $lister->getlistPath(); ?> – LSFile Project</title>
         <link rel="shortcut icon" href="<?php echo THEMEPATH; ?>/img/folder.png">
 
         <!-- STYLES -->
