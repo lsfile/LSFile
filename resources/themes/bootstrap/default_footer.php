97982f9211a6df60c83ca9627f158dd118c0b782
Eve <57066561+lsfile@users.noreply.github.com>
2019-10-27 16:22:51 +0000

    Footer

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,6 +1,6 @@
 <hr>
 
 <div class="footer">
-    © <script type="text/javascript">document.write(new Date().getFullYear());</script> <strong>LSFile Project</strong> | Connect with <a href="https://github.com/lsfile" title="GitHub LSFile" target="_blank">GitHub</a><br />
+    © <script type="text/javascript">document.write(new Date().getFullYear());</script> <strong>LSFile Project</strong><br />
     Powered by <a href="https://heroku.com" title="Heroku" target="_blank">Heroku</a>
 </div>
